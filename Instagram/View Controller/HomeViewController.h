79894a3e9d6a5f072b5771207122a9a899313be6
Angela Xu <xuangela@fb.com>
2020-07-06 15:44:50 -0700

    user can logout

---
+++ Instagram/View Controller/HomeViewController.h
@@ -0,0 +1,17 @@
+//
+//  HomeViewController.h
+//  Instagram
+//
+//  Created by Angela Xu on 7/6/20.
+//  Copyright © 2020 Angela Xu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface HomeViewController : UIViewController
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ Instagram/View Controller/HomeViewController.m
@@ -0,0 +1,44 @@
+//
+//  HomeViewController.m
+//  Instagram
+//
+//  Created by Angela Xu on 7/6/20.
+//  Copyright © 2020 Angela Xu. All rights reserved.
+//
+
+#import "HomeViewController.h"
+#import "LoginViewController.h"
+#import "SceneDelegate.h"
+#import <Parse/Parse.h>
+
+@interface HomeViewController ()
+
+@end
+
+@implementation HomeViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+- (IBAction)tapLogout:(id)sender {
+    SceneDelegate *sceneDelegate = (SceneDelegate *)self.view.window.windowScene.delegate;
+    
+    UIStoryboard *storyboard = [UIStoryboard storyboardWithName:@"Main" bundle:nil];
+    LoginViewController *loginViewController = [storyboard instantiateViewControllerWithIdentifier:@"LoginViewController"];
+    sceneDelegate.window.rootViewController = loginViewController;
+    
+    [PFUser logOutInBackgroundWithBlock:^(NSError * error) {   }];
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
